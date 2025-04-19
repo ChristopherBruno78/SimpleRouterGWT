c86104717b3bbf90c9371d6c03a4449cb4f988e2
cbruno <cbruno@cocoawerks.com>
2025-04-18 21:17:23 -0500

    added pom

--- src/main/java/com/cocoawerks/mosaic/router/client/MainView.java
+++ src/main/java/com/cocoawerks/mosaic/router/client/MainView.java
@@ -3,5 +3,4 @@ package com.cocoawerks.mosaic.router.client;
 import com.google.gwt.user.client.ui.FlowPanel;
 
 @View(route = "/")
-public class MainView extends FlowPanel {
-}
+public class MainView extends FlowPanel {}
--- src/main/java/com/cocoawerks/mosaic/router/client/Router.java
+++ src/main/java/com/cocoawerks/mosaic/router/client/Router.java
@@ -1,27 +1,25 @@
 package com.cocoawerks.mosaic.router.client;
 
 import com.google.gwt.user.client.ui.Widget;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class Router {
+  private final Map<String, Widget> views = new HashMap<>();
 
-    private final Map<String, Widget> views = new HashMap<>();
+  static final Router INSTANCE = new Router();
 
-    static final Router INSTANCE = new Router();
+  public static Router get() {
+    return INSTANCE;
+  }
 
-    public static Router get() {
-        return INSTANCE;
-    }
+  private Router() {}
 
-    private Router() {}
+  public void addRoute(String path, Widget view) {
+    views.put(path, view);
+  }
 
-    public void addRoute(String path, Widget view) {
-        views.put(path, view);
-    }
-
-    public Widget getViewForRoute(String path) {
-        return views.get(path);
-    }
+  public Widget getViewForRoute(String path) {
+    return views.get(path);
+  }
 }
