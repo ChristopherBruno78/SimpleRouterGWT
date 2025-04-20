bba345f5c7b2b776de0f5820b6e6e67382ce37be
cbruno <cbruno@cocoawerks.com>
2025-04-20 08:17:52 -0500

    format

--- src/main/java/com/cocoawerks/simplerouter/client/URL.java
+++ src/main/java/com/cocoawerks/simplerouter/client/URL.java
@@ -3,7 +3,6 @@ package com.cocoawerks.simplerouter.client;
 import static elemental2.dom.DomGlobal.window;
 
 import com.google.gwt.user.client.ui.Widget;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
